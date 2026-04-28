28c6a9cc24d1b2f6e839782ae001abfb112b40fc
kinitsZ <kinitsf@gmail.com>
2026-04-28 10:03:31 +0800

    fix loading issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,37 +2,12 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import localFont from 'next/font/local';
-
-export const sfPro = localFont({
-  src: [
-    {
-      path: '../public/fonts/SF-Pro-Display-Regular.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/SF-Pro-Display-Medium.otf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/SF-Pro-Display-Semibold.otf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/SF-Pro-Display-Bold.otf',
-      weight: '700',
-      style: 'normal',
-    },
-  ]
-})
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-poppins',
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
@@ -47,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${sfPro.className} ${poppins.variable}`}>
+      <body className={poppins.variable}>
         <ThemeProvider
           attribute='class'
           defaultTheme="light"
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 31536000,
+  },
 };
 
 export default nextConfig;
